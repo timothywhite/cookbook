ec68464825d1c37cea65b2525924a5628a614c76
Tim White <timford88@gmail.com>
2014-08-22 16:51:15 +0000

    Add recipe_link template tag

---
+++ recipes_api/templatetags/recipe_tags.py
@@ -0,0 +1,21 @@
+from django import template
+
+register = template.Library()
+
+
+def recipe_link(parser, token):
+	arguments = token.split_contents()
+	if (len(arguments) != 3):
+		raise template.TemplateSyntaxError("invalid argument length")
+		
+	return RecipeLinkNode(arguments[1], arguments[2])
+	
+class RecipeLinkNode(template.Node):
+	def __init__(self, id, text):
+		self.recipe_id = id
+		self.link_text = text
+		
+	def render(self, context):
+		return "<a class='recipe_link' href='http://thatswhatshecooked.me#!" + self.recipe_id + ":1;'>" + self.link_text + "</a>"
+		
+register.tag('recipe_link',recipe_link)
